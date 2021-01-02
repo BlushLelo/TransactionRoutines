0cba12e3d31e3c0a3eff899c79c3eeb4cd6dd998
Joao Octavio <joaooctf@gmail.com>
2021-01-02 12:32:05 -0300

    Initial commit

---
+++ src/test/java/com/pismo/test/Pismo/Test/PismoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pismo.test.Pismo.Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PismoTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
